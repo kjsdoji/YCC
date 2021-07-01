7c257f4b7c3a379560f55e88e5ddb5d1617bb7cd
Doji <kjsdoji@gmail.com>
2021-07-01 16:14:12 +0700

    Add feature for webapp

---
+++ YCC.WebApp/Models/ProductCategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YCC.ViewModels.Catalog.Categories;
+using YCC.ViewModels.Catalog.Products;
+using YCC.ViewModels.Common;
+
+namespace YCC.WebApp.Models
+{
+    public class ProductCategoryViewModel
+    {
+        public CategoryVm Category { get; set; }
+
+        public PagedResult<ProductVm> Products { get; set; }
+    }
+}
